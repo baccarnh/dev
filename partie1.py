4c4f23c831411b3acdc579c847e4c862c725954d
nour <baccarnh@gmail.com>
2019-10-16 15:33:49 +0200

    tous les exercices de la partie1

---
+++ partie1.py
@@ -0,0 +1,33 @@
+## ex1: Hello World
+print("Hello World")
+
+## ex1 avec variable
+message= "Hello World"
+print(message)
+
+## ex2: calcul divers
+print(3*3)
+print("(12/0) erreur de division par 0" )
+print(4+9+78)
+print(12-7)
+print(5e4)
+
+##ex3: communiquer avec l'ordinateur
+##print("inserer votre nom")
+nom = input("entrer votre nom")
+print("bienvenue", nom)
+
+##ex4: Nom et prénom
+nom= "BACCAR"
+prénom= "Nour Elhouda"
+print ("Bonjour ",nom, prénom)
+
+##ex5: des caractères au nombre
+myNumber= "123"
+myNumber=(int(myNumber))
+print(type(myNumber))
+
+## ex6: Majuscules et miniscules
+mavariable=input("donner votre chaine de caratères")
+print(mavariable. upper())
+print(mavariable. lower())
