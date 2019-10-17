7904385206db2f56225f31dbceb50812f92345f5
nour <baccarnh@gmail.com>
2019-10-17 11:33:31 +0200

    add ex titles in partie1.py

--- partie1.py
+++ partie1.py
@@ -1,13 +1,13 @@
-## ex1: Hello World
+print("ex1: Hello World")
 print("Hello World")
 
-## ex1 avec variable
+print ("ex1 avec variable")
 message= "Hello World"
 print(message)
 
-## ex2: calcul divers
+print("ex2: calcul divers")
 print(3*3)
-##print("(12/0) erreur de division par 0" )
+
 try: 12/0
 except ZeroDivisionError:
     print("erreur de division par0")
@@ -15,22 +15,22 @@ print(4+9+78)
 print(12-7)
 print(5e4)
 
-##ex3: communiquer avec l'ordinateur
+print("ex3: communiquer avec l'ordinateur")
 ##print("inserer votre nom")
 nom = input("entrer votre nom")
 print("bienvenue", nom)
 
-##ex4: Nom et prénom
+print("ex4: Nom et prénom")
 nom= "BACCAR"
 prénom= "Nour Elhouda"
 print ("Bonjour ",nom, prénom)
 
-##ex5: des caractères au nombre
+print("ex5: des caractères au nombre")
 myNumber= "123"
 myNumber=(int(myNumber))
 print(type(myNumber))
 
-## ex6: Majuscules et miniscules
+print("ex6: Majuscules et miniscules")
 mavariable=input("donner votre chaine de caratères")
 print(mavariable. upper())
 print(mavariable. lower())
